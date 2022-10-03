ea5e7bfb8ff7fe83a71f29cb6919000ee7443600
Evgen <zhenya.bill@gmail.com>
2022-10-03 08:31:02 +0300

    set schedule

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+
+  @Cron('45 * * * * *')
+  handleCron() {
+    this.logger.debug('Called when the second is 45');
+  }
+
+  @Interval(10000)
+  handleInterval() {
+    this.logger.debug('Called every 10 seconds');
+  }
+
+  @Timeout(5000)
+  handleTimeout() {
+    this.logger.debug('Called once after 5 seconds');
+  }
+}
