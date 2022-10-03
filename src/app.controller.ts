256ab09096c9c78be417cb5540d1793ea33262d7
Evgen <zhenya.bill@gmail.com>
2022-10-03 08:30:55 +0300

    set schedule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HtmlParserService } from "./html.parser/html.parser.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              private readonly htmlParserService:HtmlParserService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return this.htmlParserService.getEmployeeList();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { HtmlParserService } from './html.parser/html.parser.service';
+import { HttpModule } from "@nestjs/axios";
+// import { PrismaService } from "./prisma.service";
+import { TasksModule } from './src/tasks/tasks.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    HttpModule,
+    TasksModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HtmlParserService],
 })
 export class AppModule {}
